86ba7bf7daaa9baa36a77122f0560a94225bb8b6
Kuzovlev Roman <rkuzovlev@gmail.com>
2024-11-08 15:25:11 +0700

    change loader

--- src/loader.js
+++ src/loader.js
@@ -1,38 +1,39 @@
-async function loadGame() {
-    const response = await fetch('./game.png')
-    const imgArrayBuffer = await response.arrayBuffer()
+async function prepareWasm() {
+    const img = window.i
+    img.style.display = "none"
 
-    const imgBlob = new Blob( [ imgArrayBuffer ], { type: "image/png" } )
-    const imgURL = URL.createObjectURL(imgBlob)
+    const width = img.width
+    const height = img.height
+
+    const canvas = new OffscreenCanvas(width, height)
+    const context = canvas.getContext('2d')
     
-    const img = new Image()
-    img.src = imgURL
-    img.onload = async function() {
-        const canvas = new OffscreenCanvas(img.width, img.height)
-        const context = canvas.getContext('2d')
-        
-        context.drawImage(img, 0, 0)
+    context.drawImage(img, 0, 0)
 
-        const imgData = context.getImageData(0, 0, img.width, img.height)
-        
-        const imgDataView = new DataView(imgData.data.buffer)
-        const bodyLength = imgDataView.getUint16(0)
-        const wasmLength = imgDataView.getUint16(4)
+    const imgData = context.getImageData(0, 0, width, height)
+    
+    const imgDataView = new DataView(imgData.data.buffer)
+    const bodyLength = imgDataView.getUint16(0)
+    const wasmLength = imgDataView.getUint16(4)
 
-        const componentsCount = Math.ceil(bodyLength / 4)
-        const components = []
+    const componentsCount = Math.ceil(bodyLength / 4)
+    const components = []
 
-        const wasmDataWithAlpha = imgData.data.buffer.slice(8, bodyLength + 8)
-        for (let i = 0; i < componentsCount; ++i){
-            components.push(wasmDataWithAlpha.slice(i*4, i*4+3))
-        }
-
-        const componentsBlob = new Blob(components, { type: 'application/octet-stream' })
-        const wasmDataBuffer = await componentsBlob.arrayBuffer()
-        const wasmData = wasmDataBuffer.slice(0, wasmLength)
-
-        gameInit(wasmData)
+    const wasmDataWithAlpha = imgData.data.buffer.slice(8, bodyLength + 8)
+    for (let i = 0; i < componentsCount; ++i){
+        components.push(wasmDataWithAlpha.slice(i*4, i*4+3))
     }
+
+    const componentsBlob = new Blob(components)
+    const wasmDataBuffer = await componentsBlob.arrayBuffer()
+    const wasmData = wasmDataBuffer.slice(0, wasmLength)
+
+    gameInit(wasmData)
+}
+
+function loadGame() {
+    if (window.i.complete) prepareWasm()
+    else window.i.onload = prepareWasm
 }
 
 loadGame()
