3bb213eb24c1acb369b79a83dc22664e9c783c31
Kuzovlev Roman <rkuzovlev@gmail.com>
2024-11-12 19:37:42 +0700

    basic save/load for canvas and palette

--- sprite_editor/main.js
+++ sprite_editor/main.js
@@ -2,6 +2,13 @@ const canvas = document.getElementById('canvas')
 const body = document.body
 const widthInput = document.getElementById('width')
 const heightInput = document.getElementById('height')
+const savePaletteButton = document.getElementById('save_palette')
+const loadPaletteButton = document.getElementById('load_palette')
+const paletteSaveInfo = document.getElementById('palette_save_info')
+
+const saveCanvasButton = document.getElementById('save_canvas')
+const loadCanvasButton = document.getElementById('load_canvas')
+const canvasSaveInfo = document.getElementById('canvas_save_info')
 
 const context = canvas.getContext('2d')
 const cellSize = 30
@@ -12,7 +19,7 @@ let imageColors = []
 let selectedColor = 0
 let isDrawing = false
 
-const colors = Array(16)
+const colors = Array(15)
     .fill(0)
     .map((_, i) => document.getElementById(`color_${i}`))
 
@@ -20,17 +27,22 @@ colors.forEach(color => {
     const colorNumber = parseInt(color.id.split('_')[1], 10)
     const colorPreview = window[`color_preview_${colorNumber}`]
     const colorSelect = window[`color_select_${colorNumber}`]
-    colorPreview.style.backgroundColor = color.value
+    setColorForPreview(colorNumber, color.value)
     
     color.addEventListener('change', () => {
-        colorPreview.style.backgroundColor = color.value
+        setColorForPreview(colorNumber, color.value)
         colorSelect.checked = true
         selectedColor = colorNumber
         updateCanvas()
     })
 })
 
-const colorSelects = Array(16)
+function setColorForPreview(colorId, color){
+    const colorPreview = window[`color_preview_${colorId}`]
+    colorPreview.style.backgroundColor = color
+}
+
+const colorSelects = Array(15)
     .fill(0)
     .map((_, i) => document.getElementById(`color_select_${i}`))
 
@@ -39,6 +51,9 @@ colorSelects.forEach(colorSelect => {
         selectedColor = parseInt(colorSelect.id.split('_')[2], 10)
     })
 })
+
+const transparentColor = document.getElementById(`color_select_transparent`)
+transparentColor.addEventListener('change', () => selectedColor = 15)
 
 updateDimensions()
 updateCanvas()
@@ -59,6 +74,90 @@ heightInput.addEventListener('change', function(){
     if (!Number.isInteger(value) || value < 1) value = height
 
     height = value
+    updateDimensions()
+    updateCanvas()
+})
+
+savePaletteButton.addEventListener('click', function(){
+    let palette = ""
+    colors.forEach(colorInput => {
+        const color = colorInput.value.substring(1)
+        const b1 = color.substring(0, 2)
+        const b2 = color.substring(2, 4)
+        const b3 = color.substring(4)
+        palette += `\\${b1}\\${b2}\\${b3}`
+    })
+    paletteSaveInfo.style.display = "block"
+    paletteSaveInfo.textContent = palette
+})
+
+loadPaletteButton.addEventListener('click', function(){
+    const palette = prompt('insert palette wasm hex binary string')
+    const paletteBytes = palette.split('\\')
+    paletteBytes.shift()
+
+    const colorsCount = paletteBytes.length / 3
+    for (let i = 0; i < colorsCount; i++){
+        const color = "#" + paletteBytes[i * 3].toString() + paletteBytes[i * 3 + 1].toString() + paletteBytes[i * 3 + 2].toString()
+        setColorForPreview(i, color)
+        colors[i].value = color
+    }
+})
+
+saveCanvasButton.addEventListener('click', function(){
+    let canvasBytes = "\\" + height.toString(16) + "\\" + width.toString(16)
+    let lastByte = 0
+    let odd
+
+    for (let y = 0; y < height; y++){
+        for (let x = 0; x < width; x++){
+            odd = (y * width + x) % 2 === 1
+
+            const colorId = imageColors[y][x]
+            if (odd) lastByte = lastByte << 4    
+            lastByte = lastByte | colorId
+            if (odd) {
+                canvasBytes += "\\" + lastByte.toString(16)
+                lastByte = 0
+            }
+        }
+    }
+
+    if (!odd) {
+        lastByte = lastByte << 4
+        canvasBytes += "\\" + lastByte.toString(16)
+    }
+
+    canvasSaveInfo.style.display = "block"
+    canvasSaveInfo.textContent = canvasBytes
+})
+
+loadCanvasButton.addEventListener('click', function(){
+    // \3\5\33\3f\ff\ff\ff\ff\ff\f0
+    let canvasBytes = prompt('insert canvas wasm hex binary string')
+
+    canvasBytes = canvasBytes.split('\\')
+    canvasBytes.shift()
+
+    height = canvasBytes[0]
+    width = canvasBytes[1]
+
+    canvasBytes = canvasBytes.slice(2)
+
+    const colors = []
+    canvasBytes.forEach(byteString => {
+        const byte = parseInt(byteString, 16)
+        const color1 = (byte & 0xf0) >> 4
+        const color2 = byte & 0xf
+        colors.push(color1, color2)
+    })
+
+    for (let y = 0; y < height; y++){
+        for (let x = 0; x < width; x++){
+            imageColors[y][x] = colors.shift()
+        }
+    }
+
     updateDimensions()
     updateCanvas()
 })
@@ -109,7 +208,7 @@ function updateDimensions(){
             const cell = imageColors[y][x]
             
             if (!Number.isInteger(cell) || cell < 0 || cell > 15){
-                imageColors[y][x] = 0
+                imageColors[y][x] = 15
             }
         }
     }
@@ -120,7 +219,7 @@ function updateCanvas(){
         for (let x = 0; x < width; x++){
             const colorId = imageColors[y][x]
 
-            if (colorId === 0){
+            if (colorId === 15){
                 renderTransparent(x, y)
             } else {
                 const color = colors[colorId].value
@@ -131,7 +230,7 @@ function updateCanvas(){
 }
 
 function renderSelectedColor(x, y){
-    if (selectedColor === 0){
+    if (selectedColor === 15){
         renderTransparent(x, y)
     } else {
         const color = colors[selectedColor].value
@@ -140,10 +239,12 @@ function renderSelectedColor(x, y){
 }
 
 function renderTransparent(x, y){
+    context.strokeStyle = "#606060"
     context.fillStyle = "#ffffff"
     context.beginPath()
     context.rect(x * cellSize, y * cellSize, cellSize, cellSize)
     context.fill()
+    context.stroke()
 
     context.fillStyle = "#00000020"
     context.beginPath()
@@ -158,8 +259,10 @@ function renderTransparent(x, y){
 }
 
 function renderColor(x, y, color){
+    context.strokeStyle = "#606060"
     context.fillStyle = color
     context.beginPath()
     context.rect(x * cellSize, y * cellSize, cellSize, cellSize)
     context.fill()
+    context.stroke()
 }
